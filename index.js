df508b558433a17f8fdf3ea8c6a04c072e01af35
Chris Paradeise <chris.paradeise@gmail.com>
2020-01-08 22:54:12 -0800

    I think I just created a templating system

--- index.js
+++ index.js
@@ -43,4 +43,24 @@ function main() {
     ask(Manager.getQuestions());
 }
 
-main();
+// main();
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+const readFileAsync = util.promisify(fs.readFile);
+
+async function ru() {
+    let el = await readFileAsync(path.join(__dirname, "views", "engineer.html"), 'utf-8');
+
+    // fake input name
+    const member = { name: "Christopher Paradeise", role: "Engineer", email: "cparadise@gmail.com", github: "chrisp-dev" };
+    el = el.replace('{name}', member.name)
+        .replace('{role}', member.role)
+        .replace('{github}', member.github)
+        .replace('{email}', member.email);
+
+    console.log(el);
+}
+
+ru();

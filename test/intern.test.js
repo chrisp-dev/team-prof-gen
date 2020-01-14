659666738bb38ec157ae855ff9fc6f0bb76145f4
chrisp-dev <chris.paradeise@gmail.com>
2020-01-13 19:48:57 -0800

    Intern tests updated to include render fn

--- test/intern.test.js
+++ test/intern.test.js
@@ -26,4 +26,25 @@ describe("Intern class", () => {
     it("should have specific school id in questions array", () => {
         expect(Intern.getQuestions()[2].name).toBe("intern_school");
     });
+
+
+    describe('render fn', () => {
+        it('should render html with name, email, school', () => {
+            const intData = { name: "Chris", email: "chris@gmail.com", school: "chrisp-dev" };
+            const e = new Intern(intData.name, intData.email, intData.school);
+            expect(e.getName()).toBe(intData.name);
+            expect(e.getEmail()).toBe(intData.email);
+            expect(e.getSchool()).toBe(intData.school);
+
+            const htmlOutput = '<h2>' + intData.name + '</h2>\r\n' +
+                '<ul>\r\n' +
+                '    <li>role: ' + Intern.getRole() + '</li>\r\n' +
+                '    <li>email: ' + intData.email + '</li>\r\n' +
+                '    <li>school: ' + intData.school + '</li>\r\n' +
+                '</ul>'
+            e.render((err, data) => {
+                expect(data).toBe(htmlOutput);
+            });
+        });
+    });
 });

c548c78b6980a0ec1cca4e1a4794e506b7eea5f3
chrisp-dev <chris.paradeise@gmail.com>
2020-01-13 19:47:08 -0800

    Updated engineer - render function is now callback style - engineer tests are passing

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -14,4 +14,44 @@ describe("Engineer class", () => {
         const e = new Engineer();
         expect(e.getId()).toBe(1);
     });
+
+    it("should get role", () => {
+        const e = new Engineer();
+        expect(Engineer.getRole()).toBe("Engineer");
+    });
+
+    it("should get/set email", () => {
+        const e = new Engineer("Iggy", "iggy@zal.ia", "iggaz22");
+        expect(e.getEmail()).toBe("iggy@zal.ia");
+    });
+
+    it("should get/set github", () => {
+        const e = new Engineer("Iggy", "iggy@zal.ia", "iggaz22");
+        expect(e.getGithub()).toBe("iggaz22");
+    });
+
+    it('should get/set name', () => {
+        const e = new Engineer("Iggy");
+        expect(e.getName()).toBe("Iggy");
+    });
+
+    describe('render fn', () => {
+        it('should render html with name, email, github', () => {
+            const engData = { name: "Chris", email: "chris@gmail.com", github: "chrisp-dev" };
+            const e = new Engineer(engData.name, engData.email, engData.github);
+            expect(e.getName()).toBe(engData.name);
+            expect(e.getEmail()).toBe(engData.email);
+            expect(e.getGithub()).toBe(engData.github);
+
+            const htmlOutput = '<h2>' + engData.name + '</h2>\r\n' +
+                '<ul>\r\n' +
+                '    <li>role: ' + Engineer.getRole() + '</li>\r\n' +
+                '    <li>email: ' + engData.email + '</li>\r\n' +
+                '    <li>github: ' + engData.github + '</li>\r\n' +
+                '</ul>'
+            e.render((err, data) => {
+                expect(data).toBe(htmlOutput);
+            });
+        });
+    });
 });

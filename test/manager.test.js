a75e4d1cc8f147a89bad7005f6c4d553b35134ec
chrisp-dev <chris.paradeise@gmail.com>
2020-01-13 19:48:28 -0800

    Manager tests passing

--- test/manager.test.js
+++ test/manager.test.js
@@ -17,4 +17,24 @@ describe("Manager class", () => {
     it("should have specific office id in questions array", () => {
         expect(Manager.getQuestions()[2].name).toBe("manager_office");
     });
+
+    describe('render fn', () => {
+        it('should render html with name, email, office', () => {
+            const engData = { name: "Chris", email: "chris@gmail.com", office: "chrisp-dev" };
+            const e = new Manager(engData.name, engData.email, engData.office);
+            expect(e.getName()).toBe(engData.name);
+            expect(e.getEmail()).toBe(engData.email);
+            expect(e.getOffice()).toBe(engData.office);
+
+            const htmlOutput = '<h2>' + engData.name + '</h2>\r\n' +
+                '<ul>\r\n' +
+                '    <li>role: ' + Manager.getRole() + '</li>\r\n' +
+                '    <li>email: ' + engData.email + '</li>\r\n' +
+                '    <li>office: ' + engData.office + '</li>\r\n' +
+                '</ul>'
+            e.render((err, data) => {
+                expect(data).toBe(htmlOutput);
+            });
+        });
+    });
 });
